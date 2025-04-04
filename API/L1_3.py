156fe20f9e80cda7eaa3ff5d9dc842bca1f3f5d9
MikeKorsikov <nikolay.korsikov@gmail.com>
2025-04-04 16:53:42 +0200

    API

--- API/L1_3.py
+++ API/L1_3.py
@@ -4,3 +4,26 @@ import requests
 response = requests.get('http://localhost:3000/lyrics/random')
 
 print(response.text)
+
+###
+
+# Create a dictionary variable with query params
+query_params = {"artist":"Deep Purple"}
+
+# Pass the dictionary to the get() function
+response = requests.get('http://localhost:3000/lyrics/random', params=query_params)
+
+print(response.text)
+
+###
+
+# Add the `include_track` parameter
+query_params = {'artist': 'Deep Purple', 'include_track' : True}
+
+response = requests.get('http://localhost:3000/lyrics/random', params=query_params)
+
+# Print the response URL
+print(response.url)
+
+# Print the lyric
+print(response.text)
